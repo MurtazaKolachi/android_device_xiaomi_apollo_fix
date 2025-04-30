dd14f4a79418a2a4bc0647d13a14c8e09562c18e
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-04-30 15:35:06 +0500

    apollo: Pixelage bringup

--- pixelage_apollo.mk
+++ pixelage_apollo.mk
@@ -8,8 +8,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelage stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
