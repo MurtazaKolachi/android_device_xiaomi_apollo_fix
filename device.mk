9b6f9763acd5db34e3af705f28334f6e53b19a9b
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-05-02 01:39:15 +0500

    apollo: bring back MiuiCamera

--- device.mk
+++ device.mk
@@ -120,6 +120,9 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
+# Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
